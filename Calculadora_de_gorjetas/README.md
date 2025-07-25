503fe4b72795e9c6c32b73f0184d0ffb015de512
cat0blu <Cat.Blu.Roger00@gmail.com>
2025-07-24 22:10:34 -0300

    Project: Calculadora_de_gorjetas - test mobile

---
+++ Calculadora_de_gorjetas/README.md
@@ -0,0 +1,34 @@
+# PROJECT: Aplicativo de calculadora de gorjetas #
+
+## Sobre: 
+
+- . 
+
+## Ferramentas utilizada
+
+    - Html(semantica)
+    - Css e sass(estilos)
+    - Node.js(instalação de ferramentas)
+ 
+    
+## Paletas de cores
+
+### Primary
+
+- Green 400: hsl(172, 67%, 45%)
+
+### Neutral
+
+- Green 900: hsl(183, 100%, 15%)
+- Grey 500: hsl(186, 14%, 43%)
+- Grey 400: hsl(184, 14%, 56%)
+- Grey 200:                                      
+- Grey 50: hsl(189, 47%, 97%)
+- White: hsl(0, 100%, 100%)
+
+
+## Tipografias, Fontes  
+
+- Family: [Space Mono](https://fonts.google.com/specimen/Space+Mono)
+- Weights: 700
+- Font size (form inputs): 24px
---
+++ Calculadora_de_gorjetas/scripts/index.js
@@ -0,0 +1,117 @@
+let bill = document.querySelector("#cmp_BillValue > input")
+let gorjetas = document.querySelectorAll("#cmp_gorjetas > .porcentagens")
+let gorjeta_selected 
+let custom = document.getElementById("custom")
+let number_people = document.getElementById("number_people")
+let total_per_person = document.getElementById("total_per_person")
+let tip_per_person = document.getElementById("tip_per_person")
+let tip
+let total
+let btn_reset = document.getElementById("btn_reset")
+
+setInterval(() => {
+    if(bill.value.length > 10)
+    {
+        bill.value = ""
+        window.alert("Numero muito grande(max_digitos 10)")
+    }
+
+    // if(bill.value.includes("E"))
+    // {
+    //     window.alert("E presente")
+    // }
+
+}, 1000);
+
+setInterval(()=>{
+    if(custom.value.length > 10)
+    {
+        custom.value = ""
+        window.alert("Numero muito grande(max_digitos 10)")
+    }
+
+}, 1000)
+
+setInterval(()=>{
+    if(number_people.value.length > 10)
+    {
+        number_people.value = ""
+        window.alert("Numero muito grande(max_digitos 10)")
+    }
+
+}, 1000)
+
+
+btn_reset.addEventListener("click", ()=>{
+    if(window.getComputedStyle(btn_reset).opacity === "1")
+    {
+        window.location.reload()
+    }
+})
+
+custom.addEventListener("click", ()=>{
+    gorjetas.forEach((button)=>{
+            button.classList.remove("button_activate")
+        })
+    if(custom.value.length > 0 )
+    {
+        gorjeta_selected = Number(custom.value)
+
+        tip = (gorjeta_selected) / Number(number_people.value)
+        total = (Number(bill.value) + tip * Number(number_people.value)) / Number(number_people.value)
+
+        if(tip)
+        {
+            tip_per_person.innerHTML = "$" + tip.toFixed(2)
+        }
+
+        if(total)
+        {
+            total_per_person.innerHTML = "$" + total.toFixed(2)
+        }
+
+        if(tip && total)
+        {
+            btn_reset.style.opacity = "1" 
+        }
+    }
+})
+
+gorjetas.forEach((gorjeta)=>{
+
+    gorjeta.addEventListener("click", ()=>{
+        custom.value = ""
+        gorjetas.forEach((button)=>{
+            button.classList.remove("button_activate")
+        })
+
+        gorjeta.classList.add("button_activate")
+        gorjeta_selected = gorjeta.innerHTML.replace("%", " ")
+        gorjeta_selected = Number(gorjeta_selected /100)
+
+       
+        tip = (Number(bill.value) * gorjeta_selected) / Number(number_people.value)
+        total = (Number(bill.value) + tip * Number(number_people.value)) / Number(number_people.value)
+
+        console.log("bill: " + bill.value)
+        console.log("gorjeta selected: " + gorjeta_selected)
+        console.log("number_people: " + number_people.value)
+        console.log("tip: " + tip)
+        console.log("total: "+total)
+   
+        if(tip)
+        {
+            tip_per_person.innerHTML = "$" + tip.toFixed(2)
+        }
+
+        if(total)
+        {
+            total_per_person.innerHTML = "$" + total.toFixed(2)
+        }
+
+        if(tip && total)
+        {
+            btn_reset.style.opacity = "1" 
+        }
+    })
+})
